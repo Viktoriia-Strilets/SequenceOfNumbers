14a39aeba9eabdfb522ab68a190453df728ce121
Viktoriia-Strilets <Viktoriia.strilets@nure.ua>
2024-12-03 10:17:38 +0200

    added realization

--- Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: ComVisible(false)]
-
-[assembly: Guid("680e8f73-2f7e-4f89-b3fa-8c9c2e85a4f2")]
